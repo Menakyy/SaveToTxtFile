8c162591bac1c25c427f99bd30a162992bd47676
Konrad <Konrad2223@gmail.com>
2022-01-13 11:33:04 +0100

    Dodanie opisow funkcji

--- SaveToTxtFile/CSaveToTxtFile.h
+++ SaveToTxtFile/CSaveToTxtFile.h
@@ -11,7 +11,18 @@ class CSaveToTxtFile
 
 public:
 
+	/*
+		Funkcja zapisuje string do pliku tekstowego o nazwie 'fileName'
+		Ponownie uzycie funkcji spowoduje zapis kolejnego string w nowej
+		linijce
+	*/
 	void saveStringToEndFile(std::string fileName, std::string content);
+
+	/*
+	Funkcja zapisuje string do pliku tekstowego o nazwie 'fileName'
+	Ponownie uzycie funkcji spowoduje usuniecie calej zawartosci pliku
+	i wpisanie nowego stringa
+*/
 	void saveNewStringToFile(std::string fileName, std::string content);
 };
 
